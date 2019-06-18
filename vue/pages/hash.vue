7a4caf59ec43c6ff4fd153effa8096f0840c41c6
Vladislav <itochanenko@gmail.com>
2019-06-18 17:40:28 +0300

    Added template for Cuckoo Hash Page

--- vue/pages/hash.vue
+++ vue/pages/hash.vue
@@ -1,15 +1,42 @@
 <template>
     <div>
-        Хеш
+		Generte array with size of <input v-model="hashsize" type="number">
+        <button @click="initHash(hashsize)">Generate</button><br><br>
+        <input v-model="hashvalue" type="number">
+		<button @click="addKey(nodevalue)">Add</button>
+		<button @click="findKey(nodevalue)">Find</button>
+
+        <table ref="hashtable"></table>
     </div>
 </template>
 
 <script>
     module.exports = {
-        name: "hash"
+        name: "hash",
+        data: function() {
+            return {
+                hashsize : "1",
+                hashvalue : "1",
+                cuckoo: null
+            }
+        },
+        methods: {
+            addKey(value) {
+
+            },
+            findKey(value) {
+
+            },
+            initHash(value) {
+                let n = parseInt(hashsize);
+                for (let i = 0; i < n; i++) {
+                    
+                }
+            }
+        }
     };
 </script>
 
 <style scoped>
-        
+    
 </style>
